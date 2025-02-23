f57f617470817a9548883fc7a6c2838dc67cf522
Reda Lemeden <git@redalemeden.com>
2025-02-23 20:53:59 +0100

    Split drizzle dev and prod configs

--- drizzle-dev.config.ts
+++ drizzle-dev.config.ts
@@ -2,6 +2,10 @@ import type {Config} from "drizzle-kit";
 
 export default {
   dialect: "postgresql",
+  driver: "pglite",
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: "./drizzle/pgdata",
+  },
 } satisfies Config;
